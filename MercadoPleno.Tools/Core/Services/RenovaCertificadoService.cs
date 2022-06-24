c5ca5437ff03fbcbb89e20d48e3484e247ab7cdf
Bruno N Fernandes <bruno@mercadopleno.com.br>
2022-06-24 12:36:46 -0300

    ajuste das regras para renovação de certificados

--- MercadoPleno.Tools/Core/Services/RenovaCertificadoService.cs
+++ MercadoPleno.Tools/Core/Services/RenovaCertificadoService.cs
@@ -1,5 +1,6 @@
 ﻿using MercadoPleno.Tools.Core.Domains;
 using MercadoPleno.Tools.Core.Proxies.ZeroSSL.Dtos;
+using MercadoPleno.Tools.Core.Util;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ namespace MercadoPleno.Tools.Core.Services
 {
 	public class RenovaCertificadoService : CertificadoService
 	{
+		private readonly DateTime _dataLimite = DateTime.Today.AddDays(7);
 		public RenovaCertificadoService(IServiceProvider serviceProvider) : base(serviceProvider) { }
 
 		public async Task<List<ZeroSslCertificate>> RenovarCertificadoExpirando()
@@ -26,7 +28,7 @@ namespace MercadoPleno.Tools.Core.Services
 
 		protected override async Task<ZeroSslCertificate> Processar(ZeroSslUser user, ZeroSslCertificado certificado)
 		{
-			if (certificado.Status == ZeroSslStatus.Expiring_soon)
+			if ((certificado.Expires < _dataLimite) || certificado.Status.In(ZeroSslStatus.Expiring_soon, ZeroSslStatus.Expired, ZeroSslStatus.Draft))
 			{
 				await RemoverCetificado(user, certificado);
 				return await CriarCertificado(user, certificado.Domain);
